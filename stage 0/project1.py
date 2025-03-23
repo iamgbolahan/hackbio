99c7c739ab031474278386e123d3d841bfce4ed0
iamgbolahan <142104355+iamgbolahan@users.noreply.github.com>
2025-03-23 15:12:00 +0100

    Add files via upload

---
+++ stage 0/project1.py
@@ -0,0 +1,65 @@
+team_acg = {
+    "Gbolahan" : {
+        "name" : "Adegboye Omogbolahan",
+        "slack_username" : "Gbolahan",
+        "email" : "adegboyegbolahan25@gmail.com",
+        "hobby" : "Listening to music",
+        "country" : "Nigeria",
+        "discipline" : "Cell Biology and Genetics",
+        "preferred_language" : "Python"
+    },
+    "Qanitat" : {
+        "name" : "Qanitat Adedoyinmola",
+        "slack_username" : "Qanitat",
+        "email" : "qanitatakinade@gmail.com",
+        "hobby" : "sleeping",
+        "country" : "Nigeria",
+        "discipline" : "Cell Biology and Genetics",
+        "preferred_language" : "Python"
+    },
+    "Omodara" : {
+        "name" : "Ayomide Omodara",
+        "slack_username" : "Omodara",
+        "email" : "ayoboluomodara@gmail.com",
+        "hobby" : "Baking",
+        "country" : "Nigeria",
+        "discipline" : "Cell Biology and Genetics",
+        "preferred_language" : "Python"
+    },
+    "Yahya" : {
+        "name" : "Yahya Abdulrahman Babatunde",
+        "slack_username" : "Yahya",
+        "email" : "yahya.ope247@gmail.com",
+        "hobby" : "nothing",
+        "country" : "Nigeria",
+        "discipline" : "Cell Biology and Genetics",
+        "preferred_language" : "Python"
+    },
+    "Chinenyenwa" : {
+        "name" : "Chinenyenwa Mba Oji",
+        "slack_username" : "Chinenyenwa",
+        "email" : "mbaojichinenyenwa@gmail.com",
+        "hobby" : "Reading african fiction",
+        "country" : "Nigeria",
+        "discipline" : "Cell Biology and Genetics",
+        "preferred_language" : "Python"
+    },
+    "Deborah" : {
+        "name" : "Olatosin Deborah",
+        "slack_username" : "Deborah",
+        "email" : "deborah.olatosin@gmail.com",
+        "hobby" : "Baking",
+        "country" : "Nigeria",
+        "discipline" : "Cell Biology and Genetics",
+        "preferred_language" : "Python"
+        }
+}
+
+print(f"""This is everything about members of team acg : 
+        \n Chinenyenwa:{team_acg['Chinenyenwa']} 
+        \n Gbolahan: {team_acg['Gbolahan']} 
+        \n Deborah: {team_acg['Deborah']} 
+        \n Omodara: {team_acg['Omodara']}   
+        \n Yahya: {team_acg['Yahya']}
+        \n Qanitat: {team_acg['Qanitat']}
+    """)
